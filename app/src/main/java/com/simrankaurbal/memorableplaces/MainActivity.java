03906e452b08a91315300334be4aa09c2d7b1e86
Simranbal04 <simranbal01@gmail.com>
2020-02-10 18:47:28 -0500

    Layout for saving places data

---
+++ app/src/main/java/com/simrankaurbal/memorableplaces/MainActivity.java
@@ -0,0 +1,40 @@
+package com.simrankaurbal.memorableplaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listview);
+
+        final ArrayList<String> places = new ArrayList<>();
+        places.add("Add a new Place...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
